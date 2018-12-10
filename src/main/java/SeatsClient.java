45f0ff289d4635a196dc824a214cafbc8bca7fbe
Kiarahmani <rahmank@purdue.edu>
2018-12-10 16:16:57 -0500

    push to ec2

--- src/main/java/SeatsClient.java
+++ src/main/java/SeatsClient.java
@@ -29,36 +29,54 @@ public class SeatsClient {
 		}
 	}
 
-	public static void writeTransaction(Connection connect, int key, int value) throws Exception {
+	public static int decTransaction(Connection connect, int key, int value) throws Exception {
 		try {
 			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
-			PreparedStatement preparedStatement = connect.prepareStatement("update A set balance= ? where id=?");
-			preparedStatement.setInt(1, value);
-			preparedStatement.setInt(2, key);
-			preparedStatement.executeUpdate();
-			preparedStatement = connect.prepareStatement("update A set balance= ? where id=?");
-			preparedStatement.setInt(1, value * 2);
-			preparedStatement.setInt(2, key);
-			preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			throw e;
-		}
-	}
 
-	public static int readTransaction(Connection connect, int key) throws Exception {
-		int result = -1;
-		try {
-			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
 			PreparedStatement preparedStatement = connect.prepareStatement("select * from A where id=?");
 			preparedStatement.setInt(1, key);
 			ResultSet rs = preparedStatement.executeQuery();
-			if (rs.next()) {
-				result = rs.getInt("balance");
+			int oldBal = -10000;
+			if (rs.next())
+				oldBal = rs.getInt("balance");
+			// BEGIN: manually injected assertion regargin non-negativity invariant
+			if (oldBal < 0)
+				return 1;
+			// END
+
+			if (oldBal > value) {
+				preparedStatement = connect.prepareStatement("update A set balance= ? where id=?");
+				preparedStatement.setInt(1, oldBal - value);
+				preparedStatement.setInt(2, key);
+				preparedStatement.executeUpdate();
 			}
+			return 0;
+		} catch (Exception e) {
+			return -1;
+		}
+	}
+
+	public static void initTransaction(Connection connect, int key) throws Exception {
+		try {
+			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
+
+			PreparedStatement preparedStatement = connect.prepareStatement("update A set balance= 1000 where id=?");
+			preparedStatement.setInt(1, key);
+			preparedStatement.executeUpdate();
+
 		} catch (Exception e) {
 			throw e;
 		}
-		return result;
 	}
 
+	/*
+	 * public static int readTransaction(Connection connect, int key) throws
+	 * Exception { int result = -1; try {
+	 * Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
+	 * PreparedStatement preparedStatement =
+	 * connect.prepareStatement("select * from A where id=?");
+	 * preparedStatement.setInt(1, key); ResultSet rs =
+	 * preparedStatement.executeQuery(); if (rs.next()) { result =
+	 * rs.getInt("balance"); } } catch (Exception e) { throw e; } return result; }
+	 */
 }
