fcc6e8e8319adc74155cc668c40622d392c2a954
Kiarahmani <rahmank@purdue.edu>
2018-12-23 20:11:45 -0500

    table.names added

--- src/main/java/SeatsUtils.java
+++ src/main/java/SeatsUtils.java
@@ -1,6 +1,11 @@
+import java.util.concurrent.ThreadLocalRandom;
 
 public class SeatsUtils {
 	public static long getNextCustomerId() {
-		return 69L;
+		return nextLong(281474976710656L, 80501843339247631L);
+	}
+
+	public static long nextLong(long minimum, long maximum) {
+		return ThreadLocalRandom.current().nextLong(minimum, maximum);
 	}
 }
