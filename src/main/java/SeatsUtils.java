66f9ddc6c57bd88d2600d809e03bd766ad3cc709
Kiarahmani <rahmank@purdue.edu>
2018-12-23 20:42:39 -0500

    table.names added

--- src/main/java/SeatsUtils.java
+++ src/main/java/SeatsUtils.java
@@ -5,6 +5,17 @@ public class SeatsUtils {
 		return nextLong(281474976710656L, 80501843339247631L);
 	}
 
+	public static long getNextAirlineId() {
+		return nextLong(281474976710656L, 80501843339247631L);
+	}
+	
+	public static long getNextFlightId() {
+		return nextLong(89124701353L, 87750050550350462L);
+	}
+	
+	
+	
+	
 	public static long nextLong(long minimum, long maximum) {
 		return ThreadLocalRandom.current().nextLong(minimum, maximum);
 	}
