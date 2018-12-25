edde8618428dea73b77eaaad9777a7ab9cc6e42e
Kiarahmani <rahmank@purdue.edu>
2018-12-24 21:45:56 -0500

    table.names added

--- src/main/java/SeatsUtils.java
+++ src/main/java/SeatsUtils.java
@@ -9,7 +9,7 @@ public class SeatsUtils {
 
 	public static long getNextCustomerId() {
 		long depart_airport_id = ThreadLocalRandom.current().nextLong(2, 283);
-		long id = ThreadLocalRandom.current().nextLong(60000129);
+		long id = ThreadLocalRandom.current().nextLong(61110000129);
 		long composite_id = encode(new long[] { id, depart_airport_id }, COMPOSITE_BITS, COMPOSITE_POWS);
 		return composite_id;
 
