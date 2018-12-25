1e0f0cfc6a16bbb3501bea9a265cdeec4a8e4f93
Kiarahmani <rahmank@purdue.edu>
2018-12-25 15:18:35 -0500

    table.names added

--- src/main/java/SeatsUtils.java
+++ src/main/java/SeatsUtils.java
@@ -39,7 +39,7 @@ public class SeatsUtils {
 	public static long getNextFlightId() {
 		// return nextLong(89124701353L, 87750050550350462L);
 		System.out.println("~~~~~~~~>>>" + flightIds.size());
-		int index = ThreadLocalRandom.current().nextInt(1, flightIds.size() - 1);
+		int index = ThreadLocalRandom.current().nextInt(1, 33);
 		System.out.println("-------->>>"+index);
 		
 		long fid =  flightIds.get(100);
