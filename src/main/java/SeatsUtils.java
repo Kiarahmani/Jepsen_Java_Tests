ab698a5ebe9f7069e8406f515cc6cc4d29c676bf
Kiarahmani <rahmank@purdue.edu>
2018-12-31 12:50:22 -0500

    edit

--- src/main/java/SeatsUtils.java
+++ src/main/java/SeatsUtils.java
@@ -48,8 +48,10 @@ public class SeatsUtils {
 		long offset = beginDate.getTime();
 		long end = Timestamp.valueOf("2019-02-14 04:04:19").getTime();
 		long diff = end - offset + 1;
-		System.out.println("\n\n~~~~~~~~~~>>>>>"+diff+"\n");
-		diff = 4;
+		System.out.println("\n\n~~~~~~~~~~>>>>>"+diff);
+		diff = diff/3;
+		System.out.println("~~~~~~~~~~>>>>>"+diff+"\n\n");
+
 		Timestamp rand = new Timestamp(offset + (long) (Math.random() * diff));
 		return rand;
 	}
