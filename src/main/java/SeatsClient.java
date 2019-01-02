845ed77ef0f19b484ae0de8cfba2afcf87a5d7c1
Kiarahmani <rahmank@purdue.edu>
2019-01-02 13:08:49 -0500

    edit

--- src/main/java/SeatsClient.java
+++ src/main/java/SeatsClient.java
@@ -316,46 +316,46 @@ public class SeatsClient {
 	 * 
 	 */
 
-	public static int findOpenSeats(Connection conn, int f_id) throws Exception {
+	public static int findOpenSeats(Connection connect, long f_id) throws Exception {
 		try {
 
 			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
-			System.out.println(">>>>>> DOING GARBAGE");
-
-			/*
-			 * System.out.println("connecting..."); connect =
-			 * DriverManager.getConnection("jdbc:cassandra://localhost" + ":1904" + insID +
-			 * "/testks"); final long seatmap[] = new long[] { -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-			 * -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }; PreparedStatement f_stmt =
-			 * connect.prepareStatement(
-			 * "SELECT F_STATUS, F_BASE_PRICE, F_SEATS_TOTAL, F_SEATS_LEFT FROM FLIGHT WHERE F_ID = ?"
-			 * ); f_stmt.setInt(1, f_id); ResultSet f_results = f_stmt.executeQuery();
-			 * boolean adv = f_results.next(); int base_price =
-			 * f_results.getInt("F_BASE_PRICE"); int seats_left =
-			 * f_results.getInt("F_SEATS_LEFT"); int seats_total =
-			 * f_results.getInt("F_SEATS_TOTAL"); int seat_price = base_price + (base_price
-			 * * (1 - (seats_left / seats_total))); PreparedStatement s_stmt = connect
-			 * .prepareStatement("SELECT R_ID, R_F_ID, R_SEAT FROM RESERVATION WHERE R_F_ID = ?"
-			 * ); s_stmt.setInt(1, f_id); ResultSet s_results = s_stmt.executeQuery(); while
-			 * (s_results.next()) { int r_id = s_results.getInt(1); int seatnum =
-			 * s_results.getInt(3); assert (seatmap[seatnum] == -1) :
-			 * "Duplicate seat reservation: R_ID=" + r_id; seatmap[seatnum] = 1; } int ctr =
-			 * 0; Object[][] returnResults = new Object[150][]; for (int i = 0; i <
-			 * seatmap.length; ++i) { if (seatmap[i] == -1) { // Charge more for the first
-			 * seats double price = seat_price * (i < 10 ? 2.0 : 1.0); Object[] row = new
-			 * Object[] { f_id, i, price }; returnResults[ctr++] = row; if (ctr ==
-			 * returnResults.length) break; } } // FOR // print the available saets for
-			 * (Object[] o1 : returnResults) { for (Object o2 : o1) System.out.println(o2);
-			 * System.out.println("===================="); }
-			 * 
-			 */
+			  final long seatmap[] = new long[] { -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+			  -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }; 
+			  
+			  
+			  PreparedStatement f_stmt =connect.prepareStatement("SELECT F_STATUS, F_BASE_PRICE, F_SEATS_TOTAL, F_SEATS_LEFT FROM FLIGHT WHERE F_ID = ?"); 
+			  f_stmt.setLong(1, f_id); 
+			  ResultSet f_results = f_stmt.executeQuery();
+			  System.out.println(">>>>>>"+f_results.next());
+			  /*
+			  
+			  boolean adv = f_results.next(); int base_price =
+			  f_results.getInt("F_BASE_PRICE"); int seats_left =
+			  f_results.getInt("F_SEATS_LEFT"); int seats_total =
+			  f_results.getInt("F_SEATS_TOTAL"); int seat_price = base_price + (base_price
+			  * (1 - (seats_left / seats_total))); PreparedStatement s_stmt = connect
+			  .prepareStatement("SELECT R_ID, R_F_ID, R_SEAT FROM RESERVATION WHERE R_F_ID = ?"
+			  ); s_stmt.setInt(1, f_id); ResultSet s_results = s_stmt.executeQuery(); while
+			  (s_results.next()) { int r_id = s_results.getInt(1); int seatnum =
+			  s_results.getInt(3); assert (seatmap[seatnum] == -1) :
+			  "Duplicate seat reservation: R_ID=" + r_id; seatmap[seatnum] = 1; } int ctr =
+			  0; Object[][] returnResults = new Object[150][]; for (int i = 0; i <
+			  seatmap.length; ++i) { if (seatmap[i] == -1) { // Charge more for the first
+			  seats double price = seat_price * (i < 10 ? 2.0 : 1.0); Object[] row = new
+			  Object[] { f_id, i, price }; returnResults[ctr++] = row; if (ctr ==
+			  returnResults.length) break; } } // FOR // print the available saets for
+			  (Object[] o1 : returnResults) { for (Object o2 : o1) System.out.println(o2);
+			  System.out.println("===================="); }
+			  */
+			 
 
 			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
 			// TXN SUCCESSFUL!
