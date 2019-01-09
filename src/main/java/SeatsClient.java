bbee817dc206af6788be05982a812b24bf05a86e
Kiarahmani <rahmank@purdue.edu>
2019-01-09 12:30:48 -0500

    edit

--- src/main/java/SeatsClient.java
+++ src/main/java/SeatsClient.java
@@ -23,10 +23,11 @@ public class SeatsClient {
 		try {
 			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
 			for (int i = 1; i <= n; i++)
-				for (int j = 0; j < (c/500)+1; j++) {
-					CassandraConnection connect = (CassandraConnection) DriverManager
-							.getConnection("jdbc:cassandra://" + "n" + String.valueOf(i) + ":9042/seats?"
-									+ "consistency=ONE&retry=FallthroughRetryPolicy");
+				for (int j = 0; j < (c / 500) + 1; j++) {
+					CassandraConnection connect = (CassandraConnection) DriverManager.getConnection("jdbc:cassandra://"
+							+ "n" + String.valueOf(i) + ":9042/seats?" + "consistency=ONE&retry=FallthroughRetryPolicy"
+							+ "&loadbalancing=TokenAwarePolicy(DCAwareRoundRobinPolicy(\"dc_n " + String.valueOf(i)
+							+ "\"))");
 					connectionPool.add(connect);
 				}
 		} catch (ClassNotFoundException e) {
