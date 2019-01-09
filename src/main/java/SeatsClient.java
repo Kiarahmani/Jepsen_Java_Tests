94769d10b1df95c18ce18f040bce62889340654a
Kiarahmani <rahmank@purdue.edu>
2019-01-08 22:04:06 -0500

    edit

--- src/main/java/SeatsClient.java
+++ src/main/java/SeatsClient.java
@@ -20,7 +20,7 @@ public class SeatsClient {
 			Class.forName("com.github.adejanovski.cassandra.jdbc.CassandraDriver");
 			System.out.println("SeatsClient.java: Connecting to Cassandra on: " + localAddr);
 			//&loadbalancing=TokenAwarePolicy(DCAwareRoundRobinPolicy('dc_n1'))
-			connect = (CassandraConnection) DriverManager.getConnection("jdbc:cassandra://172.31.1.189:9042/seats?debug=true");
+			connect = (CassandraConnection) DriverManager.getConnection("jdbc:cassandra://172.31.1.189:9042/seats?debug=true&loadbalancing=RoundRobinPolicy()");
 			System.out.println("XXX"+connect.getClusterMetadata().getClusterName());
 		} catch (SQLException e) {
 			e.printStackTrace();
