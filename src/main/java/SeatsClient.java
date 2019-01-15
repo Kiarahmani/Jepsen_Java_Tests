d6d5dbae4abe2d281777a7b7fd19ebec7e391ad4
Kiarahmani <rahmank@purdue.edu>
2019-01-15 11:19:12 -0500

    edit

--- src/main/java/SeatsClient.java
+++ src/main/java/SeatsClient.java
@@ -52,17 +52,25 @@ public class SeatsClient {
 	 * 
 	 */
 
-	public static int deposit(CassandraConnection conn, int id, int bal) throws Exception {
+	public static int deposit_checking(CassandraConnection conn, int id, int bal) throws Exception {
 		try {
 			PreparedStatement stmt = conn.prepareStatement("SELECT balance FROM checking WHERE id = ? ");
-			;
+			PreparedStatement stmt2 = conn.prepareStatement("SELECT balance FROM total WHERE id = ? ");
 			stmt.setInt(1, id);
+			stmt2.setInt(1, id);
 			ResultSet results = stmt.executeQuery();
+			ResultSet results2 = stmt2.executeQuery();
 			int old_bal = results.getInt("balance");
+			int old_bal2 = results2.getInt("balance");
 			stmt = conn.prepareStatement("UPDATE checking SET balance = ? WHERE id = ? ");
 			stmt.setInt(1, old_bal + bal);
 			stmt.setInt(2, id);
 			stmt.executeUpdate();
+			
+			stmt2 = conn.prepareStatement("UPDATE total SET balance = ? WHERE id = ? ");
+			stmt2.setInt(1, old_bal2 + bal);
+			stmt2.setInt(2, id);
+			stmt2.executeUpdate();
 
 			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
 			// TXN SUCCESSFUL!
@@ -73,5 +81,37 @@ public class SeatsClient {
 			return -1;
 		}
 	}
+	
+	
+	public static int deposit_saving(CassandraConnection conn, int id, int bal) throws Exception {
+		try {
+			PreparedStatement stmt = conn.prepareStatement("SELECT balance FROM saving WHERE id = ? ");
+			PreparedStatement stmt2 = conn.prepareStatement("SELECT balance FROM total WHERE id = ? ");
+			stmt.setInt(1, id);
+			stmt2.setInt(1, id);
+			ResultSet results = stmt.executeQuery();
+			ResultSet results2 = stmt2.executeQuery();
+			int old_bal = results.getInt("balance");
+			int old_bal2 = results2.getInt("balance");
+			stmt = conn.prepareStatement("UPDATE saving SET balance = ? WHERE id = ? ");
+			stmt.setInt(1, old_bal + bal);
+			stmt.setInt(2, id);
+			stmt.executeUpdate();
+			
+			stmt2 = conn.prepareStatement("UPDATE total SET balance = ? WHERE id = ? ");
+			stmt2.setInt(1, old_bal2 + bal);
+			stmt2.setInt(2, id);
+			stmt2.executeUpdate();
+
+			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+			// TXN SUCCESSFUL!
+			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+			return 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+	
 	
 }
