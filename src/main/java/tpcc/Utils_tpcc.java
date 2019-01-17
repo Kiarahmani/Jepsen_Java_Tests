a6b28b5c9acd7083ef2ba5f35868797ac9325ea1
Kiarahmani <rahmank@purdue.edu>
2019-01-17 13:09:21 -0500

    edit

--- src/main/java/tpcc/Utils_tpcc.java
+++ src/main/java/tpcc/Utils_tpcc.java
@@ -1,18 +1,33 @@
 package tpcc;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Utils_tpcc {
+
+	static AtomicBoolean atomicInitialized = new AtomicBoolean(false);
+	static boolean waitForInit = true;
 
 	// this function will be -dynamically- called from clojure at runtime
 	public Utils_tpcc() {
 		Utils_tpcc.initialize();
 	}
 
-	
 	/*
 	 * 
 	 * 
 	 */
 	public static void initialize() {
-		System.out.println("Utils_tpcc: intializing data structures....");
+		if (atomicInitialized.compareAndSet(false, true)) {
+			System.out.println("Utils_tpcc: intializing data structures....");
+			waitForInit = false;
+
+		} else {
+			while (waitForInit)
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+		}
 	}
 }
