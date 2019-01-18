481d0d8d20e3c54908a3ad09f55a95d212055796
Kiarahmani <rahmank@purdue.edu>
2019-01-18 15:51:00 -0500

    edit

--- src/main/java/tpcc/NewOrder.java
+++ src/main/java/tpcc/NewOrder.java
@@ -143,10 +143,9 @@ public class NewOrder {
 
 				// retrieve stock
 				stmt = conn.prepareStatement(
-						"SELECT S_REMOTE_CNT, S_ORDER_CNT,S_YTD, S_QUANTITY, S_DATA, S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, "
-								+ "       S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10" + "  FROM " + "STOCK"
+						"SELECT *" + "  FROM " + "STOCK"
 								+ " WHERE S_I_ID = ? " + "   AND S_W_ID = ?");
-				stmt.setInt(1, ol_i_id ); 
+				stmt.setInt(1, ol_i_id );  
 				stmt.setInt(2, ol_supply_w_id );
 				ResultSet s_rs = stmt.executeQuery();
 				if (!s_rs.next()) {
