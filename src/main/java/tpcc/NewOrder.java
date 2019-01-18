43753dad1499f99cfd509d866cd9b6eb81929c77
Kiarahmani <rahmank@purdue.edu>
2019-01-18 11:48:58 -0500

    edit

--- src/main/java/tpcc/NewOrder.java
+++ src/main/java/tpcc/NewOrder.java
@@ -9,9 +9,15 @@ import com.github.adejanovski.cassandra.jdbc.CassandraConnection;
 public class NewOrder {
 	public static int newOrder(CassandraConnection conn, int w_id, int d_id, int c_id, int o_all_local, int o_ol_cnt,
 			int[] itemIDs, int[] supplierWarehouseIDs, int[] orderQuantities) throws Exception {
-		PreparedStatement stmt = null;
+		PreparedStatement stmt = null, stmtUpdateStock = null;
 
 		try {
+			// datastructures required for bookkeeping
+			double[] itemPrices = new double[o_ol_cnt];
+			String[] itemNames = new String[o_ol_cnt];
+			int[] stockQuantities = new int[o_ol_cnt];
+			double[] orderLineAmounts = new double[o_ol_cnt];
+			double total_amount = 0;
 			// retrieve w_tax rate
 			stmt = conn.prepareStatement("SELECT W_TAX " + "  FROM " + "WAREHOUSE" + " WHERE W_ID = ?");
 			stmt.setInt(1, w_id);
@@ -83,6 +89,90 @@ public class NewOrder {
 			stmt.setInt(2, d_id);
 			stmt.setInt(3, w_id);
 			stmt.executeUpdate();
+			// For each O_OL_CNT item on the order perform the following tasks
+			for (int ol_number = 1; ol_number <= o_ol_cnt; ol_number++) {
+				int ol_supply_w_id = supplierWarehouseIDs[ol_number - 1];
+				int ol_i_id = itemIDs[ol_number - 1];
+				int ol_quantity = orderQuantities[ol_number - 1];
+				// retrieve item
+				stmt = conn
+						.prepareStatement("SELECT I_PRICE, I_NAME , I_DATA " + "  FROM " + "ITEM" + " WHERE I_ID = ?");
+				stmt.setInt(1, ol_i_id);
+				ResultSet i_rs = stmt.executeQuery();
+				// this is expected to happen 1% of the times
+				if (!i_rs.next()) {
+					if (ol_number != o_ol_cnt) {
+						System.out.println("ERROR_14: Invalid item id: (" + ol_i_id
+								+ ") given in the middle of the order list (unexpected)");
+						return 14;
+					}
+					System.out.println("EXPECTED_ERROR_15: Invalid item id: (" + ol_i_id + ")");
+					return 15;
+				}
+				double i_price = i_rs.getDouble("I_PRICE");
+				String i_name = i_rs.getString("I_NAME");
+				String i_data = i_rs.getString("I_DATA");
+				i_rs.close();
+				itemPrices[ol_number - 1] = i_price;
+				itemNames[ol_number - 1] = i_name;
+
+				// retrieve stock
+				stmt = conn.prepareCall(
+						"SELECT S_REMOTE_CNT, S_ORDER_CNT,S_YTD, S_QUANTITY, S_DATA, S_DIST_01, S_DIST_02, S_DIST_03, S_DIST_04, S_DIST_05, "
+								+ "       S_DIST_06, S_DIST_07, S_DIST_08, S_DIST_09, S_DIST_10" + "  FROM " + "STOCK"
+								+ " WHERE S_I_ID = ? " + "   AND S_W_ID = ?");
+				stmt.setInt(1, ol_i_id);
+				stmt.setInt(2, ol_supply_w_id);
+				ResultSet s_rs = stmt.executeQuery();
+				if (!s_rs.next()) {
+					System.out.println("ERROR_16: Invalid stock primary key: (" + ol_i_id + "," + ol_supply_w_id + ")");
+					return 16;
+				}
+				int s_quantity = s_rs.getInt("S_QUANTITY");
+				int s_ytd = s_rs.getInt("S_YTD");
+				int s_order_cnt = s_rs.getInt("S_ORDER_CNT");
+				int s_remote_cnt = s_rs.getInt("S_REMOTE_CNT");
+				String s_data = s_rs.getString("S_DATA");
+				String s_dist_01 = s_rs.getString("S_DIST_01");
+				String s_dist_02 = s_rs.getString("S_DIST_02");
+				String s_dist_03 = s_rs.getString("S_DIST_03");
+				String s_dist_04 = s_rs.getString("S_DIST_04");
+				String s_dist_05 = s_rs.getString("S_DIST_05");
+				String s_dist_06 = s_rs.getString("S_DIST_06");
+				String s_dist_07 = s_rs.getString("S_DIST_07");
+				String s_dist_08 = s_rs.getString("S_DIST_08");
+				String s_dist_09 = s_rs.getString("S_DIST_09");
+				String s_dist_10 = s_rs.getString("S_DIST_10");
+				s_rs.close();
+				//
+				stockQuantities[ol_number - 1] = s_quantity;
+				if (s_quantity - ol_quantity >= 10) {
+					s_quantity -= ol_quantity; // new s_quantity
+				} else {
+					s_quantity += -ol_quantity + 91; // new s_quantity
+				}
+				int s_remote_cnt_increment;
+				if (ol_supply_w_id == w_id) {
+					s_remote_cnt_increment = 0;
+				} else {
+					s_remote_cnt_increment = 1;
+				}
+				// update stock row
+				stmtUpdateStock = conn.prepareCall("UPDATE " + "STOCK" + " SET S_QUANTITY = ?," + "S_YTD = ?,"
+						+ "S_ORDER_CNT = ?," + "S_REMOTE_CNT = ? " + " WHERE S_I_ID = ? " + "   AND S_W_ID = ?");
+				stmtUpdateStock.setInt(1, s_quantity);
+				stmtUpdateStock.setInt(2, s_ytd + ol_quantity);
+				stmtUpdateStock.setInt(3, s_order_cnt + 1);
+				stmtUpdateStock.setInt(4, s_remote_cnt + s_remote_cnt_increment);
+				stmtUpdateStock.setInt(5, ol_i_id);
+				stmtUpdateStock.setInt(6, ol_supply_w_id);
+				stmtUpdateStock.addBatch();
+				//
+				double ol_amount = ol_quantity * i_price;
+				orderLineAmounts[ol_number - 1] = ol_amount;
+				total_amount += ol_amount;
+			}
+			stmtUpdateStock.executeBatch();
 
 			//
 			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
