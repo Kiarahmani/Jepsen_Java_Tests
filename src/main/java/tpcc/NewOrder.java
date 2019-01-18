361d022a7d3a07cef37e1a90a979616fb7308567
Kiarahmani <rahmank@purdue.edu>
2019-01-18 15:33:26 -0500

    edit

--- src/main/java/tpcc/NewOrder.java
+++ src/main/java/tpcc/NewOrder.java
@@ -13,14 +13,34 @@ public class NewOrder {
 
 		try {
 			System.out.println("******************************");
-			System.out.println(w_id);
-			System.out.println(d_id);
-			System.out.println(c_id);
-			System.out.println(o_all_local);
-			System.out.println(o_ol_cnt);
-			System.out.println(itemIDs);
-			System.out.println(supplierWarehouseIDs);
-			System.out.println(orderQuantities);
+			System.out.println("w_id:" + w_id);
+			System.out.println("d_id" + d_id);
+			System.out.println("c_id" + c_id);
+			System.out.println("o_all_local" + o_all_local);
+			System.out.println("o_ol_cnt" + o_ol_cnt);
+			System.out.print("itemIDs: [");
+			String delim = "";
+			for (int i : itemIDs) {
+				System.out.print(delim + i);
+				delim = ", ";
+			}
+			System.out.println("]");
+
+			System.out.print("supplierWarehouseIDs: [");
+			delim = "";
+			for (int i : supplierWarehouseIDs) {
+				System.out.print(delim + i);
+				delim = ", ";
+			}
+			System.out.println("]");
+
+			System.out.print("orderQuantities: [");
+			delim = "";
+			for (int i : orderQuantities) {
+				System.out.print(delim + i);
+				delim = ", ";
+			}
+			System.out.println("]");
 
 			System.out.println("******************************");
 			// datastructures required for bookkeeping
