8cc634a2647a9ed43798e500e3e2827f96fdfc3c
Kiarahmani <rahmank@purdue.edu>
2019-01-18 14:11:19 -0500

    edit

--- src/main/java/tpcc/Utils_tpcc.java
+++ src/main/java/tpcc/Utils_tpcc.java
@@ -1,33 +1,35 @@
 package tpcc;
 
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Utils_tpcc {
 
 	static AtomicBoolean atomicInitialized = new AtomicBoolean(false);
 	static boolean waitForInit = true;
+	static int scale = -10;
 
 	// this function will be -dynamically- called from clojure at runtime
 	public Utils_tpcc(int scale) {
+		Utils_tpcc.scale = scale;
 		Utils_tpcc.initialize(scale);
 	}
 
 	/*
 	 * 
+	 * INITIALIZATION CODE
 	 * 
 	 */
 	public static void initialize(int scale) {
 		if (atomicInitialized.compareAndSet(false, true)) {
-			System.out.println("Utils_tpcc_"+scale+": intializing data structures....");
-			
+			System.out.println("Utils_tpcc_" + scale + ": intializing data structures....");
+
 			/*
 			 * 
 			 * THIS IS WHERE YOUR INITIALIZATION CODE GOES
 			 * 
 			 */
-			
-			
-			
+
 			waitForInit = false;
 		} else {
 			while (waitForInit)
@@ -37,5 +39,15 @@ public class Utils_tpcc {
 					e.printStackTrace();
 				}
 		}
+	}
+
+	/*
+	 * 
+	 * AUXILIARY CODE
+	 * 
+	 */
+
+	public static int get_w_id() {
+		return ThreadLocalRandom.current().nextInt(1, scale);
 	}
 }
