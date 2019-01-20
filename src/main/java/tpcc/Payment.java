1546d8ef7850b6303063bbcbf14dc2fb7c855f0f
Kiarahmani <rahmank@purdue.edu>
2019-01-19 19:11:25 -0500

    edit

--- src/main/java/tpcc/Payment.java
+++ src/main/java/tpcc/Payment.java
@@ -4,8 +4,10 @@ import com.github.adejanovski.cassandra.jdbc.CassandraConnection;
 
 public class Payment {
 	public static int payment(CassandraConnection conn, int w_id, int d_id, boolean customerByName, int c_id,
-			String c_last, int customerDistrictID, int paymentAmount) throws Exception {
+			String c_last, int customerDistrictID, double paymentAmount) throws Exception {
 		try {
+			boolean isRemote = (w_id != customerDistrictID);
+
 			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
 			// TXN SUCCESSFUL!
 			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
