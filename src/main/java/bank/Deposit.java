817a8e9d2592e4c6ef60276bdc0d3bef39989ca3
Kiarahmani <rahmank@purdue.edu>
2019-02-13 12:59:16 -0500

    bank added

---
+++ src/main/java/bank/Deposit.java
@@ -0,0 +1,29 @@
+package bank;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.github.adejanovski.cassandra.jdbc.CassandraConnection;
+
+public class Deposit {
+	public static int deposit(CassandraConnection conn, int id, int amount) throws Exception {
+		try {
+			PreparedStatement stmt = conn.prepareStatement("SELECT balance FROM accounts WHERE id = ? ");
+			stmt.setInt(1, id);
+			ResultSet results = stmt.executeQuery();
+			int old_bal = results.getInt("balance");
+			//
+			stmt = conn.prepareStatement("UPDATE accounts SET balance = ? WHERE id = ? ");
+			stmt.setInt(1, old_bal + amount);
+			stmt.setInt(2, id);
+			stmt.executeUpdate();
+			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+			// TXN SUCCESSFUL!
+			// ❄❄❄❄❄❄❄❄❄❄❄❄❄❄❄
+			return 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return -1;
+		}
+	}
+}
